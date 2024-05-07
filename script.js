e5214dc8f958666213b5f44023e98bcf3b57ae94
Louis Chavez <67764701+chavez62@users.noreply.github.com>
2024-05-07 15:53:37 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const teamName = document.getElementById('team');
+const worldCupYear = document.getElementById('year');
+const headCoach = document.getElementById('head-coach');
+const playerCards = document.getElementById('player-cards');
+const playersDropdownList = document.getElementById('players');
+const myFavoriteFootballTeam = {
+    team: 'Argentina',
+    sport: 'Football',
+    year: 1986,
+    isWorldCupWinner : true,
+    headCoach: {
+        coachName: 'Carlos Bilardo',
+        matches: 7,
+    },
+    players: [{name: "Sergio Almirón",
+    position: "forward",
+    number: 1,
+    isCaptain: false,
+    nickname: null}, {name: "Sergio Batista",
+    position: "midfielder",
+    number: 2,
+    isCaptain: false,
+    nickname: null }] 
+};
